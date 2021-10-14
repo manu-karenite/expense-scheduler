26e04802a2b8e826bf0cde9cf57fe66c2109b033
Manavesh Narendra <manavesh.narendra@gmail.com>
2021-10-14 12:05:18 +0530

    Added Conditional Message

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,19 +12,19 @@ function Expenses(pass)
     //console.log(event.target.value);
     setYear(event);
   }
+
+  let expenseContent = <p>No Expenses Found</p>;
+  if(pass.pass.length>0)
+  expenseContent=pass.pass.map(function(el,index)
+        {
+          return  <ExpenseItem key={el.id} title={el.title } price={el.amount} date={el.date}></ExpenseItem>
+        })
   
   console.log(pass.pass.length);
     return (
         <Card className="expenses">
         <ExpenseFilter selected={year} onChangeFilter={onChangeHandler}/>
-      {
-       
-        pass.pass.map(function(el,index)
-        {
-          return  <ExpenseItem key={el.id} title={el.title } price={el.amount} date={el.date}></ExpenseItem>
-        })
-      
-      }
+        {expenseContent}
         </Card>
     )
 }
